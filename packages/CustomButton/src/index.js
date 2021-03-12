49bf6c004c52e503ed2c10aa024b1b9e06ab2834
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-03-12 17:15:02 +0100

    added new changes

--- packages/CustomButton/src/index.js
+++ packages/CustomButton/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+console.log("adding new changes")
+
 const Button = ({ type = 'button', children, onClick }) => (
   <div>
     <button type={type} className="button" onClick={onClick}>
