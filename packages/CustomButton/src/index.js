a05a064c39626cd9c886e7413d539e08c1248d04
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-03-12 17:02:45 +0100

    changed names for packages

--- packages/CustomButton/src/index.js
+++ packages/CustomButton/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-console.log("adding extra lines to be log")
-
 const Button = ({ type = 'button', children, onClick }) => (
   <div>
     <button type={type} className="button" onClick={onClick}>
