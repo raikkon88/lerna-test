985e75b9e77d3ff4edb48a17ce17193977e2b913
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-03-19 12:35:36 +0100

    feat: Added a change to provoque lerna version changes

--- packages/CustomButton/src/index.js
+++ packages/CustomButton/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-console.log("adding new changes")
-
 const Button = ({ type = 'button', children, onClick }) => (
   <div>
     <button type={type} className="button" onClick={onClick}>
