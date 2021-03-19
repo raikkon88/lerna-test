7c916dd2f20b7153a820386ffb306c2abad3a66b
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-03-19 15:43:09 +0100

    feat: Added basic resources into the readme file.

--- packages/CustomButton/src/index.js
+++ packages/CustomButton/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-console.log("added new change")
-
 const Button = ({ type = 'button', children, onClick }) => (
   <div>
-    <button type={type} className="button" onClick={onClick}>
+    <button type={type} className="button" onClick={onClick} >
       {children}
     </button>
   </div>
